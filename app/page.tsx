6aac9aa43cc96af4d6d133fba7dc5491c9cc9092
saulm23 <marcasaul00@gmail.com>
2025-06-03 22:49:36 -0400

    Added TopBar.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import BottomNavBar from "@/components1/BottomNavBar"
 import TopBar from "@/components1/TopBar"
 import React from "react"
 export default function Home() {
   return (
   <div>
     <TopBar/>
+    <BottomNavBar/>
   </div>
   )
 }
--- components1/TopBar.tsx
+++ components1/TopBar.tsx
@@ -1,24 +1,30 @@
-'use client'
 import Image from 'next/image'
-import React from 'react'
-import {Bell} from 'lucide-react'
 import Link from 'next/link'
-const TopBar = () => (
-    <div className="w-full flex justify-between items-center px-4 py-3 bg-[#120029]">
-        <div className='flex justify-center flex-1'>
-            <Image
-                src="/logo.svg"
-                alt='CineXStream Logo'
-                width={140}
-                height={40}
-                className='object-contain' />
-            <Link href="/pages/notifications" className="relative">
-                <Bell size={34} className="text-white" />
-                <span className="absolute -top-1 -right-1 h-2 bg-red-500 rounded-full"/>
-            </Link>
-        </div>
+import { Bell } from 'lucide-react'
 
+const TopBar = () => (
+  <div className="w-full flex items-center justify-between px-4 py-3 bg-[#120029]">
+    {/* Left Spacer (for symmetry or optional back button) */}
+    <div className="w-10" />
+
+    {/* Centered Logo */}
+    <div className="flex justify-center flex-1">
+      <Image
+        src="/logo.svg"
+        alt="CineXStream Logo"
+        width={140}
+        height={40}
+        className="object-contain"
+      />
     </div>
+
+    <div className="w-10 flex justify-end">
+      <Link href="/notifications" className="relative">
+        <Bell size={28} className="text-white" />
+        <span className="absolute top-0 right-0 h-2 w-2 bg-red-500 rounded-full" />
+      </Link>
+    </div>
+  </div>
 )
 
 export default TopBar
