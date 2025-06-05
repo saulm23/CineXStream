906c5d259e7bf77e71b89bdebf162bc31d9a7504
saulm23 <marcasaul00@gmail.com>
2025-06-05 00:06:31 -0400

    Added Login and SignUp Layout

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -3,7 +3,7 @@ const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_0962b827._.js");
 runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__4c58c055._.js");
 runtime.loadChunk("server/chunks/ssr/app_1f3630ef._.js");
-runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__012ba519._.js");
+runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__111216ed._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_96715ba7._.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_client_components_forbidden-error_ea7ea172.js");
 runtime.loadChunk("server/chunks/ssr/node_modules_next_dist_client_components_unauthorized-error_c8949b27.js");
--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++ .next/static/chunks/app_layout_tsx_c0237562._.js
@@ -1,7 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css"
+  "static/chunks/[root-of-the-server]__9d126714._.css",
+  "static/chunks/_c43c7ad4._.js"
 ],
     source: "dynamic"
 });
