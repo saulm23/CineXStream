9678f25382ab13b40f9c6ee6bfdbfb579c637b10
saulm23 <marcasaul00@gmail.com>
2025-06-05 00:41:04 -0400

    Added Input Field Component

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,11 +1,28 @@
-import Button from '@/components1/Button'
-import TopBar from '@/components1/TopBar'
-import React from 'react'
+import Button from "@/components1/Button";
+import InputField from "@/components1/InputText";
+import React from "react";
 
-const Signup = () => {
+const signup = () => {
   return (
-    <h1>signup</h1>
-  )
-}
+    <div className="mt-24 bg-black/50 py-10 px-6 md:mt-0 md:max-w-sm md_px-14">
+      <form>
+        <h1 className="text-3xl font-semi-bold">Signup</h1>
+        <InputField
+          type="email"
+          name="email"
+          placeholder="Correo electrónico"
+        />
+        <InputField
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          className="mt-4"
+        />
+        <Button text="SignUp" href="/" className="bg-[#333] bg-[#51360]" />
+      </form>
+      <h1> Ya tienes una cuenta?</h1>
+    </div>
+  );
+};
 
-export default Signup
+export default signup;
