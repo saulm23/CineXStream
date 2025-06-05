a5cb4f1f2655f8e3f60bd86c0a81a31d7df79d2b
Fabricio Vera <verafabricio953@gmail.com>
2025-06-05 10:10:44 -0400

    Pagina CombosPage agregado (se reemplazo el Combos.tsx por CombosPage.tsx) y se agregaron sus componntes

---
+++ pages/CombosPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import BackButton from "@/components1/BackButton";
+import SearchButton from "@/components1/SearchButton";
+import Combos from "@/components1/Combos";
+const CombosPage = () => {
+  return (
+    <div>
+        <div>
+            <BackButton/>
+            <h1>Combos</h1>
+            <SearchButton/>
+        </div>
+        <h1>Candy Bar</h1>
+        <div>
+          <Combos/>
+        </div>
+        <button>Realizar Compra</button>
+    </div>
+  )
+}
+
+export default CombosPage
