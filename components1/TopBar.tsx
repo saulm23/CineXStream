14b39df549e375a4fd6ef14681c62fbf3c96e722
saulm23 <marcasaul00@gmail.com>
2025-06-05 18:43:52 -0400

    Added Search Button in Top Bar

--- components1/TopBar.tsx
+++ components1/TopBar.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Bell } from 'lucide-react'
+import { Bell, Search } from 'lucide-react'
 
 const TopBar = () => (
   <div className="w-full flex items-center justify-between px-4 py-3 bg-[#120029]">
-    <div className="w-10" />
+    <div className="cursor-pointer w-10 flex justify-start">
+      <Link href="/search" className="relative">
+        <Search size = {28} className="text-white"/>
+        <span className="absolute top-0 right-0 h-2 w-2"></span>
+      
+      </Link>
+    </div>
 
     <div className="flex justify-center flex-1">
       <Image
@@ -18,7 +24,7 @@ const TopBar = () => (
 
     <div className="w-10 flex justify-end">
       <Link href="/notifications" className="relative">
-        <Bell size={28} className="text-white" />
+        <Bell size={28} className="cursor-pointer text-white" />
         <span className="absolute top-0 right-0 h-2 w-2 bg-red-500 rounded-full" />
       </Link>
     </div>
